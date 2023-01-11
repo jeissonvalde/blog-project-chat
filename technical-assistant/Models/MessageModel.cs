53ef3e1b7e12aece114f45768f4b119e114f2eb5
Jeisson A Valderrama <jeissonvalderramam@gmail.com>
2023-01-11 18:22:28 -0500

    To Prepare Controllers and First Models for handle the database.

---
+++ technical-assistant/Models/MessageModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace technical_assistant.Models
+{
+    public class MessageModel
+    {
+        public DateTime Timestamp { get; set; }
+
+        [Required(ErrorMessage ="Es necesario conocer el emisor del mensaje.")]
+        public string TransmitterId { get; set; }
+
+        public string TransmitterName { get; set; }
+
+        [Required(ErrorMessage ="Es necesario conocer para quién va dirigido el mensaje.")]
+        public string ReceiverId { get; set; }
+
+        public string ReceiverName { get; set; }
+
+        [Required(ErrorMessage ="No está permitido un mensaje sin contenido.")]
+        public string Content { get; set; }
+    }
+}
